4e353f6ef59bb9e1b120fa061183dcb1cf0714c4
Deepak <dkmiitg@gmail.com>
2022-04-24 01:19:47 +0530

    profile updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { ProfileComponent } from './profile/profile.component';
+import { EducationComponent } from './education/education.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import {MatListModule} from '@angular/material/list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    EducationComponent,
+    ExperiencesComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +34,15 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    MatTabsModule,
+    MatListModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatFormFieldModule ,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
