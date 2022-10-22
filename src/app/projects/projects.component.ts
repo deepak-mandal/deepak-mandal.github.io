617b8a95e332739c481084708204ee661f60900b
Deepak <dkmiitg@gmail.com>
2022-10-22 16:58:41 +0530

    profile updates

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   wordCloudImg = 'assets/images/tech.png'
   wordCloudAlt = 'Software Engineer, CGI || B.Tech, IIT Guwahati'
+  pythonGame = 'assets/images/Project-Fantasy_Cricket_Game_Evaluation.png';
 }
