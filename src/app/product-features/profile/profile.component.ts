ffc10b38c8206d53852310369a5bd67ca3dfe56a
Deepak Mandal <dkmiitg@gmail.com>
2025-08-18 13:58:31 +0530

    made total experiences dynamic

--- src/app/product-features/profile/profile.component.ts
+++ src/app/product-features/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PROFILE } from '../constant/product-feature-constant';
 
 @Component({
@@ -6,13 +6,26 @@ import { PROFILE } from '../constant/product-feature-constant';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
- 
+export class ProfileComponent implements OnInit{
 
+  totalExperiences!: number;
   profileTitle: string = PROFILE.DEEPAK_MANDAL
   profileSubTitle: string = PROFILE.CGI_IITG
   profileContentData: string = PROFILE.DESCRIPTION
   profilePath: string = PROFILE.PRFILE_PIC
+
+
+  ngOnInit(): void {
+    const startDate = new Date(2021, 8, 27); // Months are 0-based: 8 = September
+    const today = new Date();
+    const diffYears = (today.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24 * 365.25);
+    this.totalExperiences = Number(diffYears.toExponential(1));
+
+  }
+  
+ 
+
+
  
  
 
