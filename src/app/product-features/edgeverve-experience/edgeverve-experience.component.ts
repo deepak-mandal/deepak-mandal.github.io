a4e704dd8f169551ddac1323d60ec724d6c0bdda
Deepak Mandal <dkmiitg@gmail.com>
2026-06-14 15:54:00 +0530

    updated

--- src/app/product-features/edgeverve-experience/edgeverve-experience.component.ts
+++ src/app/product-features/edgeverve-experience/edgeverve-experience.component.ts
@@ -1,12 +1,13 @@
 import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardAvatar, MatCardHeader, MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { EXPERIENCE } from '../constant/product-feature-constant';
 import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'dkm-edgeverve-experience',
-  imports: [MatStepperModule, MatCardModule, MatCardHeader, MatCardAvatar, NgStyle],
+  imports: [MatStepperModule, MatCardModule, MatCardHeader, MatCardAvatar, MatIconModule, NgStyle],
   templateUrl: './edgeverve-experience.component.html',
   styleUrl: './edgeverve-experience.component.scss',
 })
